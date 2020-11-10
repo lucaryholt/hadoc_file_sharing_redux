bce06b38c635af0a686d576470152fb44acd2ec2
Luca Ryholt <lucaryholt@gmail.com>
2020-11-10 07:41:19 +0100

    delete in repo impl

--- repo/Repo.js
+++ repo/Repo.js
@@ -9,9 +9,7 @@ const insertPromise = async function (collectionName, data) {
             const collection = db.collection(collectionName);
 
             collection.insertOne(data, (error, result) => {
-                if (error) {
-                    reject();
-                }
+                if (error) reject();
 
                 client.close();
 
@@ -30,9 +28,7 @@ const findPromise = async function (collectionName, query) {
             const collection = db.collection(collectionName);
 
             collection.find(query).toArray((error, result) => {
-                if (error) {
-                    reject();
-                }
+                if (error) reject();
 
                 client.close();
 
@@ -51,9 +47,7 @@ const updatePromise = async function (collectionName, query, data) {
             const collection = db.collection(collectionName);
 
             collection.updateOne(query, { $set: data }, (error, result) => {
-                if (error) {
-                    reject();
-                }
+                if (error) reject();
 
                 resolve(result);
             });
@@ -61,20 +55,64 @@ const updatePromise = async function (collectionName, query, data) {
     });
 }
 
-async function find(collectionName, query){
+const deleteOnePromise = async function (collectionName, query) {
+    return new Promise((resolve, reject) => {
+        MongoDB.connect(process.env.DB_SERVER_CONNECTION_STRING, { useUnifiedTopology: true }, (error, client) => {
+            if (error) throw new Error();
+
+            const db = client.db(process.env.DB_NAME);
+            const collection = db.collection(collectionName);
+
+            collection.deleteOne(query, (error, result) => {
+                if (error) reject();
+
+                resolve(result);
+            });
+        });
+    });
+}
+
+const deleteManyPromise = async function (collectionName, query) {
+    return new Promise((resolve, reject) => {
+        MongoDB.connect(process.env.DB_SERVER_CONNECTION_STRING, { useUnifiedTopology: true }, (error, client) => {
+            if (error) throw new Error();
+
+            const db = client.db(process.env.DB_NAME);
+            const collection = db.collection(collectionName);
+
+            collection.deleteMany(query, (error, result) => {
+                if (error) reject();
+
+                resolve(result);
+            });
+        });
+    });
+}
+
+async function find(collectionName, query) {
     return await findPromise(collectionName, query);
 }
 
-async function insert(collectionName, data){
+async function insert(collectionName, data) {
     return await insertPromise(collectionName, data);
 }
 
-async function update(collectionName, query, data){
+async function update(collectionName, query, data) {
     return await updatePromise(collectionName, query, data);
+}
+
+async function deleteOne(collectionName, query) {
+    return await deleteOnePromise(collectionName, query);
+}
+
+async function deleteMany(collectionName, query) {
+    return await deleteManyPromise(collectionName, query);
 }
 
 module.exports = {
     find,
     insert,
-    update
+    update,
+    deleteOne,
+    deleteMany
 }
